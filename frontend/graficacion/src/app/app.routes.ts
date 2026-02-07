a9fc4f74974417bfa22c5ff8630266e08187eab3
aleee-lc <el_alex_2004@hotmail.com>
2026-02-07 09:51:20 -0700

    Creado proyecto angular

---
+++ frontend/graficacion/src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Landing } from './pages/landing/landing';
+import { Home } from './pages/home/home';
+import { AppShell } from './pages/app-shell/app-shell';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'landing', pathMatch: 'full' },
+  { path: 'landing', component: Landing },
+  { path: 'login', component: Login },
+  { path: 'home', component: Home },
+  { path: 'app-shell', component: AppShell },
+];
---
+++ frontend/graficacion/src/app/pages/app-shell/app-shell.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-app-shell',
+  imports: [RouterOutlet],
+  templateUrl: './app-shell.html',
+  styleUrl: './app-shell.css',
+})
+export class AppShell {
+
+}
